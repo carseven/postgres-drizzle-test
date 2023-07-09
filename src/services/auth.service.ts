830b176b916da7c0341c5d92f6eff708d5a5f62b
carseven <carles.serra@hotmail.es>
2023-07-09 13:41:24 +0200

    feat: implement basic auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -18,6 +18,36 @@ export class AuthService {
         private readonly loggerService: LoggerService,
     ) {}
 
+    public async validateAuth(authorization: string | undefined): Promise<boolean> {
+        if (!authorization) {
+            return false;
+        }
+
+        const removedBearer = authorization.replace('Bearer', '').trim();
+
+        // Transform from base64 to json
+        let requestAuth: unknown;
+        try {
+            requestAuth = JSON.parse(Buffer.from(removedBearer, 'base64').toString());
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+
+        // Validate schema with zod
+        const authParsed = AuthRequest.safeParse(requestAuth);
+        if (!authParsed.success) {
+            return false;
+        }
+
+        // TODO: Encrypt password for security and implement session token and expiration policies
+        // /login, refresh token and logout
+
+        // Check valid auth (Probably good idea to add cache layer)
+        const auth = authParsed.data;
+        return await this.validateEmailAndPassword(auth.email, auth.password);
+    }
+
     public async validateEmailAndPassword(email: string, password: string): Promise<boolean> {
         try {
             const user = await this.dbHelper.db
